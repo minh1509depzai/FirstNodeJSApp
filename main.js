da0b3d7b4214f93899dfa740dc52df57785c7b2b
minh1509depzai <minh0356648407@gmail.com>
2024-10-03 22:54:05 +0700

    Update the URL routing.

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
 const http = require('http');
+// const url = require('url')
 
 //////////////////////SERVER////////////////////
 const server = http.createServer((req, res) =>{
+    console.log(req.url);
+    switch (req.url)
+    {
+        case '/MinhNC28' :
+            console.log('Someone is calling MinhNC');
+            break;
+        case '/favicon.ico':
+            //This case to rude out the /favicon.ico URL that the browser always send.
+            break;
+        default :
+            console.log('Someone is at the default state of the page');
+            break;
+    }
+        
     res.end('Hello from server');
 });
 
