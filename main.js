b82795caa68d53808870ba95f95bfe6f9798e5e8
minhminhminh1509 <76503692+minhminhminh1509@users.noreply.github.com>
2024-10-08 22:30:06 +0700

    Update code so that it can send a HTML file from server

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 const http = require('http');
 // const url = require('url')
 
+const testHtmlFile = fs.readFileSync('./index.html', 'utf-8')
+
 //////////////////////SERVER////////////////////
 const server = http.createServer((req, res) =>{
     console.log(req.url);
     switch (req.url)
     {
         case '/MinhNC28' :
+            res.end(testHtmlFile)
             console.log('Someone is calling MinhNC');
             break;
         case '/favicon.ico':
